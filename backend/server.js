adafd054667e8a2d277881ffd3c3c9e89653ea69
unknown <OTMLCR@NTB3982A.corp.oktedi.internal>
2021-10-06 02:40:52 +1000

    commit

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRoute from './routes/user.js'
+import authRoute from './routes/auth.js'
+import productRoute from './routes/product.js'
+import cartRoute from './routes/cart.js'
+import orderRoute from './routes/order.js'
+import stripeRoute from './routes/stripe.js'
+import cors from 'cors'
+
+const app = express()
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('DB Connection Successfull!'))
+  .catch((err) => {
+    console.log(err)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
+app.use('/api/products', productRoute)
+app.use('/api/carts', cartRoute)
+app.use('/api/orders', orderRoute)
+app.use('/api/checkout', stripeRoute)
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Backend server is running!')
+})
